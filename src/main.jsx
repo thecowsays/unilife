1c24cd4d84094317c9b5cb424de897b63a091534
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-22 00:17:09 -0400

    added vite, rtr dom, axios, icons, modal, cleanup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react-swc";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);

a84d9e3144299c1fc5a6b264f17fb0cad2741f18
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-23 11:23:38 -0400

    updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // optimizeDeps: {
-  // exclude: ["@swc/core-win32-x64-msvc"],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
 });

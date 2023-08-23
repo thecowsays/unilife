7a29d23c654b84a05b72de17f25adaae3156cdb1
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-22 22:55:50 -0400

    added imports

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,16 @@
-// import react from "@vitejs/plugin-react-swc";
+import Header from "./../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 import "./Homepage.module.css";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Header />
+      Homepage
+      <Footer />
+    </div>
+  );
 }
 
 export default Homepage;

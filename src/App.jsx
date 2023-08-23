cc23f4d1156a6eac09b7c5b96b8a9109137b02e7
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-22 22:56:04 -0400

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage/Homepage";
+
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/" element={<Homepage />} /> */}
+        {/* <Route path="/" element={<Homepage />} /> */}
+        {/* <Route path="/" element={<Homepage />} /> */}
+        {/* <Route path="/" element={<Homepage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

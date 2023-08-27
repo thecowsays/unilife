f2c600f41f4758104ef74878d7bcbe758d0dcb48
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-26 22:16:42 -0400

    created Slider jsx,css

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,14 @@
+import SliderCSS from "./Slider.module.css";
+
+function Slider() {
+  return (
+    <div className={SliderCSS.container}>
+      <div>
+        <h1>Sample Heading</h1>
+        <p>Sample paragraph</p>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;

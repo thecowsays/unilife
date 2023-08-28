18de63105695f43ad14b5563ffead3df48854f66
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-27 23:23:11 -0400

    wip - dynamic headings

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,21 @@
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
 import SliderCSS from "./Slider.module.css";
+import { useEffect } from "react";
 
 function Slider() {
+  // I want to show different text depending on which page is viewed.
+  // How can I determine the current page? -- useLocation
+  const location = useLocation();
+  const { path } = location;
+
+  useEffect(() => {
+    if (path === "/SeeAllCities") {
+      return;
+    }
+  }, []);
+
   return (
     <div className={SliderCSS.container}>
       <div>

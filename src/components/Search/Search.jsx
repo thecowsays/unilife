abe6a39b3f36b3ae2485d4e8b2775031f6c502b2
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-28 14:40:55 -0400

    WIP - Search (pagination)

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,42 @@
 import axios from "axios";
 
-import "./Search.module.css";
+import SearchCSS from "./Search.module.css";
+import { useEffect, useState } from "react";
 
 function Search() {
-    return <div>
-        Search
+  // when I select a city, I want to see the details for that city
+  // how to select a city? build the ui
+  // endpoint: https://unilife-server.herokuapp.com/cities
+  const [cities, setCities] = useState([]);
 
-  </div>;
+  useEffect(() => {
+    // make api call to find all city names
+    axios
+      .get(`https://unilife-server.herokuapp.com/cities`)
+      .then((res) => {
+        // console.log(res.data.response);
+        const citiesList = [];
+
+        res?.data?.response?.map((city) => {
+          citiesList.push(city.name);
+          setCities(citiesList);
+        });
+        // console.log(`my array: ${citiesList}`);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+    
+
+  return (
+    <div className={SearchCSS.container}>
+      <select>
+        {cities.map((city, index) => (
+          <option key={index}>{city}</option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default Search;
